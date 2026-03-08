46da88406d81a6fc103fecf4d9ea5e1a45c536c5
Treston Fallavollita <ioj11@txstate.edu>
2026-03-08 12:09:24 -0500

    SQUASH: GridPlacer Node Created
    Node Created with simple script to attach a grid.
    
    grid_placer: Necessary Variables Inits
    
    Since the grid_placer relies on the class being provided a grid and a node to place, these variables are checked immediately to ensure they aren't null.
    
    grid_placer: Method To Place Nodes on Grid
    
    The placeNode() will now place a node at the position of the mouse pointer. Clicking however does not currently call this function.
    
    grid_placer: Clicking Adds Nodes
    
    Now when the left mouse button is clicked, a new node will be created.
    
    grid_placer: Moved Null Checkers
    
    Null checking will only occur when user clicks. This is to allow devs more freedom, while still ensuring that when the node is being placed there isn't a null value.
    
    grid_placer: Disallow Overlapping Nodes
    
    Option to disable overlaps.

---
+++ Nodes/grid_placer.gd
@@ -0,0 +1,53 @@
+class_name grid_placer
+extends Node2D
+
+@export
+var active : bool = true
+
+@export_category("Dependencies")
+@export
+var grid : moveable_grid = null
+@export
+var node : PackedScene = null
+
+@export
+var allowOverlap : bool = false
+
+#This feels like a bool, but actually it will track mouse Pos to dertermine
+#	if the mouse was clicked or dragged.
+var clicking : Vector2 = Vector2.ZERO
+
+func _input(event):
+	if active:
+		if event is InputEventMouseButton:
+			if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+				clicking = get_viewport().get_mouse_position()
+			if event.is_released() and event.button_index == MOUSE_BUTTON_LEFT:
+				if get_viewport().get_mouse_position().distance_to(clicking) < 0.5:
+					placeNode()
+
+func placeNode():
+	checkNulls()
+	var placeNode : Node2D = node.instantiate()
+	var mousePos = get_global_mouse_position()
+	var snapMouse = snapPosToGrid(mousePos)
+	
+	if not allowOverlap and is_overlap(snapMouse,get_tree().get_nodes_in_group("onGrid")):
+		return
+	
+	placeNode.add_to_group("onGrid")
+	grid.add_child(placeNode)
+	placeNode.position = snapPosToGrid(mousePos)
+	
+func snapPosToGrid(pos : Vector2):
+	return pos.snappedf(grid.GRID_DISTANCE)
+
+func is_overlap(pos, group):
+	for i in group:
+		if i.position == pos:
+			return true
+	return false
+
+func checkNulls():
+	assert(grid != null, "Class relies on a grid")
+	assert(node != null, "Class relies on a provided Scene to place")
