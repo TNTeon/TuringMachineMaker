7dd07742a542e28ad2c3c97564dff4c78cafb7db
Treston Fallavollita <ioj11@txstate.edu>
2026-03-07 19:27:49 -0600

    grid_placer: Const Variables Added
    Const variables added to adjust settings of the grid.

--- Nodes/grid_placer.gd
+++ Nodes/grid_placer.gd
@@ -1,4 +1,8 @@
+class_name grid_placer
 extends Node2D
+
+const LINE_THICKNESS = 10.0
+const GRID_DISTANCE = 100.0
 
 func _process(delta):
 	queue_redraw()
@@ -9,16 +13,20 @@ func drawGrid():
 	var leftX = make_canvas_position_local(Vector2(0,0)).x
 	var rightX = make_canvas_position_local(get_viewport_rect().size).x
 	
-	var vertXStart = leftX-fmod(leftX,100)
-	var horYStart = topY-fmod(topY,100)
+	var vertXStart = leftX-fmod(leftX,GRID_DISTANCE)
+	var horYStart = topY-fmod(topY,GRID_DISTANCE)
 	
 	var horizontalLines = 0
-	while(vertXStart + 100*horizontalLines < rightX):
-		draw_line(Vector2(vertXStart + 100*horizontalLines, topY), Vector2(vertXStart + 100*horizontalLines, bottomY), Color.BLACK, 10)
+	var horLinePos = vertXStart + GRID_DISTANCE*horizontalLines
+	while(horLinePos < rightX):
+		horLinePos = vertXStart + GRID_DISTANCE*horizontalLines
+		draw_line(Vector2(horLinePos, topY), Vector2(horLinePos, bottomY), Color.BLACK, LINE_THICKNESS)
 		horizontalLines += 1
 	var verticalLines = 0
-	while(horYStart + 100*verticalLines < bottomY):
-		draw_line(Vector2(leftX, horYStart + 100*verticalLines), Vector2(rightX, horYStart + 100*verticalLines), Color.BLACK, 10)
+	var virtLinePos = horYStart + GRID_DISTANCE * verticalLines
+	while(virtLinePos < bottomY):
+		virtLinePos = horYStart + GRID_DISTANCE * verticalLines
+		draw_line(Vector2(leftX, virtLinePos), Vector2(rightX, virtLinePos), Color.BLACK, LINE_THICKNESS)
 		verticalLines += 1
 	
 func _draw():
